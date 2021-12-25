ede5dd63b81e1964ab1c877e8892b662b2c74f59
fizioterapia <cubasa4@gmail.com>
2021-12-25 16:07:40 +0100

    get player info + javascript

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+    $apikey = '<add your api key here>';
+
+    if (isset($_GET['sid'])) {
+        $steamid = $_GET['sid'];
+    } else {
+        $steamid = '76561197960279927';
+    }
+
+    $api = json_decode(file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . $apikey . "&steamids=" . $steamid), true);
+
+    $authserver = bcsub( $steamid, '76561197960265728' ) & 1;
+    $authid = ( bcsub($steamid, '76561197960265728') - $authserver ) / 2;
+    $steamid32 = "STEAM_0:$authserver:$authid";
+
+    $avatar = $api['response']['players'][0]['avatarfull'];
+    $player_name = $api['response']['players'][0]['personaname'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Loading Screen</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+  </head>
+  <body>
+    <div id="background">
+      <img
+        src="https://cdnb.artstation.com/p/assets/images/images/024/538/827/original/pixel-jeff-clipa-s.gif?1582740711"
+      />
+      <div class="overlay"></div>
+    </div>
+    <div id="loading">
+      <div class="server--information">
+        <h1>kozak gaming</h1>
+        <div class="server--map">
+          You're currently playing <span id="gamemode">sandbox</span> on
+          <span id="mapname">gm_construct</span>.
+        </div>
+        <div class="progress">
+          <span id="percent" class="progress--inner"> 0% </span>
+        </div>
+        <span id="status" class="progress--title"
+          >Retrieving server info...</span
+        >
+      </div>
+      <div class="player--information">
+        <div class="player--avatar">
+          <img src="<?php echo $avatar; ?>" />
+        </div>
+        <div class="player--userinfo">
+          <div class="inner">
+            <span class="player--username"> <?php echo $player_name; ?> </span>
+            <span class="player--steamid"> <?php echo $steamid32; ?> </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="assets/js/loading.js"></script>
+  </body>
+</html>
