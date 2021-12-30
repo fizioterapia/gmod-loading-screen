a114a212c831b55b0c0b5499cfc43579d984e6c5
fizi <cubasa4@gmail.com>
2021-12-30 14:36:17 +0100

    add backstretch and some images

---
+++ assets/js/loading.js
@@ -0,0 +1,78 @@
+var neededFiles;
+var downloadedFiles = 0;
+
+var loadingPercent = document.getElementById("percent");
+var loadingStatus = document.getElementById("status");
+
+var blurinit = false;
+function GameDetails(
+  servername,
+  serverurl,
+  mapname,
+  maxplayers,
+  steamid,
+  gamemode
+) {
+  document.getElementById("mapname").innerHTML = mapname;
+  document.getElementById("gamemode").innerHTML = gamemode;
+}
+
+function DownloadingFile(fileName) {
+  downloadedFiles++;
+  refreshProgress();
+
+  setStatus("Downloading files...");
+}
+
+function SetStatusChanged(status) {
+  if (status.indexOf("Getting Addon #") != -1) {
+    downloadedFiles++;
+    refreshProgress();
+  } else if (status == "Sending client info...") {
+    setProgress(100);
+  } else if (status == "Starting Lua...") {
+    setProgress(90);
+  } else if (status == "Workshop Complete") {
+    setProgress(85);
+  } else if (status == "No files to request!") {
+    setProgress(85);
+  }
+
+  setStatus(status);
+}
+
+function setStatus(text) {
+  loadingStatus.innerHTML = text;
+}
+
+function SetFilesNeeded(needed) {
+  neededFiles = needed + 1;
+}
+
+function refreshProgress() {
+  progress = Math.floor((downloadedFiles / neededFiles) * 80);
+
+  setProgress(progress);
+}
+
+function setProgress(progress) {
+  loadingPercent.innerHTML = progress + "%";
+}
+
+/*
+  Backstretch
+*/
+
+var images = [
+  "assets/img/1.jpg",
+  "assets/img/2.jpg",
+  "assets/img/3.jpg",
+  "assets/img/4.jpg",
+];
+
+var options = {
+  duration: 3000,
+  fade: 750,
+};
+
+$("#background").backstretch(images, options);
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    $apikey = '<add your api key here>';
+
+    if (isset($_GET['sid'])) {
+        $steamid = $_GET['sid'];
+    } else {
+        $steamid = '76561197960279927';
+    }
+
+    $api = json_decode(file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . $apikey . "&steamids=" . $steamid), true);
+
+    $authserver = bcsub( $steamid, '76561197960265728' ) & 1;
+    $authid = ( bcsub($steamid, '76561197960265728') - $authserver ) / 2;
+    $steamid32 = "STEAM_0:$authserver:$authid";
+
+    $avatar = $api['response']['players'][0]['avatarfull'];
+    $player_name = $api['response']['players'][0]['personaname'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Loading Screen</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+  </head>
+  <body>
+    <div id="background">
+      <div class="overlay"></div>
+    </div>
+    <div id="loading">
+      <div class="server--information">
+        <h1>kozak gaming</h1>
+        <div class="server--map">
+          You're currently playing <span id="gamemode">sandbox</span> on
+          <span id="mapname">gm_construct</span>.
+        </div>
+        <div class="progress">
+          <span id="percent" class="progress--inner"> 0% </span>
+        </div>
+        <span id="status" class="progress--title"
+          >Retrieving server info...</span
+        >
+      </div>
+      <div class="player--information">
+        <div class="player--avatar">
+          <img src="<?php echo $avatar; ?>" />
+        </div>
+        <div class="player--userinfo">
+          <div class="inner">
+            <span class="player--username"> <?php echo $player_name; ?> </span>
+            <span class="player--steamid"> <?php echo $steamid32; ?> </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.1.18/jquery.backstretch.min.js" integrity="sha512-bXc1hnpHIf7iKIkKlTX4x0A0zwTiD/FjGTy7rxUERPZIkHgznXrN/2qipZuKp/M3MIcVIdjF4siFugoIc2fL0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="assets/js/loading.js"></script>
+  </body>
+</html>
